3eeb81aa6a5ced53c8ccbb11d3e35c3fc74a4d92
DannZolik <danjuha1@gmail.com>
2024-12-19 15:48:50 +0200

    Clean code

--- snake.js
+++ snake.js
@@ -106,15 +106,6 @@ function intersectRect(r1, r2) {
       r2.height < r1.y);
   }
 
-function touches(a, b) {
-	// has horizontal gap
-	if (a.x1 > b.x2 || b.x1 > a.x2) return false;
-
-	// has vertical gap
-	if (a.y1 > b.y2 || b.y1 > a.y2) return false;
-
-	return true;
-}
 
 function render() {
     drawingSurface.clearRect(0, 0, canvas.width, canvas.height);
